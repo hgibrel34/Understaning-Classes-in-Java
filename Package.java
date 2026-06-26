b77a1df78c7fcc659b341e565dd87c535f2cb2a1
hgibrel34 <123712372+hgibrel34@users.noreply.github.com>
2026-06-25 21:50:30 -0400

    Create Package.java

---
+++ Package.java
@@ -0,0 +1,36 @@
+public class Package {
+    private static int numPackages = 0;
+    private Destination destination;
+    private String itemName;
+    private double price;
+    private boolean onSale;
+
+    // Constructor: applies 6% tax on price
+    public Package(String name, double p, Destination d) {
+        itemName = name;
+        price = p * 1.06;
+        onSale = false;
+        destination = new Destination(d.getCity(), d.getState(), d.getZipcode());
+        numPackages++;
+    }
+
+    public Destination getDestination() { return destination; }
+    public String getItemName() { return itemName; }
+    public double getPrice() { return price; }
+    public boolean getOnSale() { return onSale; }
+
+    public void setDestination(Destination d) { destination = new Destination(d.getCity(), d.getState(), d.getZipcode()); }
+    public void setItemName(String n) { itemName = n; }
+    public void setPrice(double p) { price = p; }
+    public void setOnSale(boolean s) { onSale = s; }
+
+    // Applies 20% discount if on sale
+    public void salePrice() { if (onSale) price *= 0.8; }
+
+    // Equal if same item name and destination
+    public boolean equals(Package other) { return itemName.equals(other.itemName) && destination.equals(other.destination); }
+
+    public String toString() { return String.format("Item: %s | Price: $%.2f | Dest: %s", itemName, price, destination); }
+
+    public static int getNumPackages() { return numPackages; }
+}
